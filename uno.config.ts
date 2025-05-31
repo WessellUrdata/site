3cfbc9bb431a178fc90eacce98f436fd7b92099d
Wessell <git@wesl.cc>
2025-05-31 11:06:03 +0800

    chore(deps): bump deps and fix unocss breaking changes

--- uno.config.ts
+++ uno.config.ts
@@ -1,8 +1,8 @@
-import { defineConfig, presetUno, presetTypography, transformerCompileClass } from "unocss";
+import { defineConfig, presetWind3, presetTypography, transformerCompileClass } from "unocss";
 
 export default defineConfig({
   presets: [
-    presetUno(),
+    presetWind3(),
     presetTypography({
       cssExtend: {
         ":root": {
